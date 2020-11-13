50391162b07ba48835619bfbaa37e1a69ba73d5f
EnochStar <1181591005@zust.edu.cn>
2020-11-13 14:48:02 +0800

    99. 恢复二叉搜索树

---
+++ src/main/java/tree/RecoverBinarySearchTree.java
@@ -0,0 +1,31 @@
+package tree;
+
+/**
+ * @author FL8000
+ * 给你二叉搜索树的根节点 root ，该树中的两个节点被错误地交换。请在不改变其结构的情况下，恢复这棵树。
+ *
+ * 进阶：使用 O(n) 空间复杂度的解法很容易实现。你能想出一个只使用常数空间的解决方案吗？
+ *
+ * 链接：https://leetcode-cn.com/problems/recover-binary-search-tree
+ */
+public class RecoverBinarySearchTree {
+    TreeNode pre,t1,t2;
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+        int temp = t1.val;
+        t1.val = t2.val;
+        t2.val = temp;
+    }
+    public void inorder(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        inorder(root.left);
+        if(pre != null && pre.val > root.val) {
+            if(t1 == null) t1 = pre;
+            t2 = root;
+        }
+        pre = root;
+        inorder(root.right);
+    }
+}
