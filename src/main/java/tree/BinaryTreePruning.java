4f9a1d29d314e313b784e6be8c929aefe18e3e2b
EnochStar <1181591005@zust.edu.cn>
2020-11-19 17:19:18 +0800

    814. 二叉树剪枝

--- src/main/java/tree/BinaryTreePruning.java
+++ src/main/java/tree/BinaryTreePruning.java
@@ -22,4 +22,15 @@ package tree;
  * 链接：https://leetcode-cn.com/problems/binary-tree-pruning
  */
 public class BinaryTreePruning {
+    public TreeNode pruneTree(TreeNode root) {
+        if(root == null) {
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if(root.left == null && root.right == null && root.val == 0){
+            return null;
+        }
+        return root;
+    }
 }
