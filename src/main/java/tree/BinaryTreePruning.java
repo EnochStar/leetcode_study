1c00bb71fd630172806b5103d64f943c68a0227b
EnochStar <1181591005@zust.edu.cn>
2020-11-19 17:18:59 +0800

    814. 二叉树剪枝

---
+++ src/main/java/tree/BinaryTreePruning.java
@@ -0,0 +1,25 @@
+package tree;
+
+/**
+ * @author FL8000
+ * 给定二叉树根结点root，此外树的每个结点的值要么是 0，要么是 1。
+ *
+ * 返回移除了所有不包含 1 的子树的原二叉树。
+ *
+ * ( 节点 X 的子树为 X 本身，以及所有 X 的后代。)
+ *
+ * 示例1:
+ * 输入: [1,null,0,0,1]
+ * 输出: [1,null,0,null,1]
+ *
+ * 示例2:
+ * 输入: [1,0,1,0,0,0,1]
+ * 输出: [1,null,1,null,1]
+ *
+ * 解释:
+ * 只有红色节点满足条件“所有不包含 1 的子树”。
+ * 右图为返回的答案。
+ * 链接：https://leetcode-cn.com/problems/binary-tree-pruning
+ */
+public class BinaryTreePruning {
+}
