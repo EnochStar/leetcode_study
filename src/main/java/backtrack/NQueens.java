c920b7c327c23ddb44bc5ade5deee286da9521d3
EnochStar <1181591005@zust.edu.cn>
2020-11-30 17:12:35 +0800

    51. N 皇后

---
+++ src/main/java/backtrack/NQueens.java
@@ -0,0 +1,98 @@
+package backtrack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author FL8000
+ * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ *
+ * 给定一个整数 n，返回所有不同的n 皇后问题的解决方案。
+ *
+ * 每一种解法包含一个明确的n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ *
+ * 
+ *
+ * 示例：
+ *
+ * 输入：4
+ * 输出：[
+ *  [".Q..",  // 解法 1
+ *   "...Q",
+ *   "Q...",
+ *   "..Q."],
+ *
+ *  ["..Q.",  // 解法 2
+ *   "Q...",
+ *   "...Q",
+ *   ".Q.."]
+ * ]
+ * 解释: 4 皇后问题存在两个不同的解法。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/n-queens
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class NQueens {
+    List<List<String>> res = new ArrayList();
+    public List<List<String>> solveNQueens(int n) {
+        // 初始化数组
+        char[][] board = new char[n][n];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = '.';
+            }
+        }
+        backtrack(board,0);
+        return res;
+    }
+    // 回溯模板
+    void backtrack(char[][] board, int row) {
+        if(row == board.length) {
+            res.add(convert(board));
+            return;
+        }
+        int n = board[row].length;
+        for(int j = 0;j < n;j++) {
+            if(!isValid(board, row, j)) {
+                continue;
+            }
+            board[row][j] = 'Q';
+            backtrack(board,row + 1);
+            board[row][j] = '.';
+        }
+    }
+    // 剪枝
+    boolean isValid(char[][] board, int row, int col) {
+        int n = board.length;
+        // 检查列是否有皇后互相冲突
+        for (int i = 0; i < n; i++) {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+        // 检查右上方是否有皇后互相冲突
+        for (int i = row - 1, j = col + 1;
+             i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+        // 检查左上方是否有皇后互相冲突
+        for (int i = row - 1, j = col - 1;
+             i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+        return true;
+    }
+    // 直接将二维数组的行转化成String  之前不知道能这样操作
+    List<String> convert(char[][] board) {
+        List<String> list = new LinkedList<>();
+        for (int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            list.add(s);
+        }
+        return list;
+    }
+}
