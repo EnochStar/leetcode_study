c98ac8a759418d004797448a6181a127d3d034c9
EnochStar <1181591005@zust.edu.cn>
2020-12-13 11:15:03 +0800

    60. 排列序列

--- src/main/java/backtrack/PermutationSequence.java
+++ src/main/java/backtrack/PermutationSequence.java
@@ -8,9 +8,10 @@ import java.util.List;
  * @author EnochStar
  * @title: PermutationSequence
  * @projectName leetcode_study
- * @description: The set [1, 2, 3, ...,?n] contains a total of n! unique permutations.
+ * @description:
+ * 给出集合 [1,2,3,...,n]，其所有元素共有 n! 种排列。
  *
- * By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+ * 按大小顺序列出所有排列情况，并一一标记，当 n = 3 时, 所有排列如下：
  *
  * "123"
  * "132"
@@ -18,25 +19,25 @@ import java.util.List;
  * "231"
  * "312"
  * "321"
- * Given n and k, return the kth permutation sequence.
+ * 给定 n 和 k，返回第 k 个排列。
  *
+ *  
  *
+ * 示例 1：
  *
- * Example 1:
+ * 输入：n = 3, k = 3
+ * 输出："213"
+ * 示例 2：
  *
- * Input: n = 3, k = 3
- * Output: "213"
- * Example 2:
+ * 输入：n = 4, k = 9
+ * 输出："2314"
+ * 示例 3：
  *
- * Input: n = 4, k = 9
- * Output: "2314"
- * Example 3:
+ * 输入：n = 3, k = 1
+ * 输出："123"
+ *  
  *
- * Input: n = 3, k = 1
- * Output: "123"
- *
- *
- * Constraints:
+ * 提示：
  *
  * 1 <= n <= 9
  * 1 <= k <= n!
