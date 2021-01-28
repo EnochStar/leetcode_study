0d5f52c53c445046d5c63fb6ea332dbbb919b0b8
EnochStar <1181591005@zust.edu.cn>
2021-01-28 19:35:30 +0800

    25. K 个一组翻转链表

---
+++ src/main/java/com/bjx/linkedList/ReverseKGroup.java
@@ -0,0 +1,58 @@
+package com.bjx.linkedList;
+
+/**
+ * @author EnochStar
+ * @title: ReverseKGroup
+ * @projectName leetcode_study
+ * @description: 
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+ *
+ * k 是一个正整数，它的值小于或等于链表的长度。
+ *
+ * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 给你这个链表：1->2->3->4->5
+ *
+ * 当 k = 2 时，应当返回: 2->1->4->3->5
+ *
+ * 当 k = 3 时，应当返回: 3->2->1->4->5
+ *
+ *  
+ *
+ * 说明：
+ *
+ * 你的算法只能使用常数的额外空间。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-nodes-in-k-group
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021/1/28 16:21
+ */
+public class ReverseKGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr = head;
+        int count = 0;
+        while (curr != null && count != k) { // find the k+1 node
+            curr = curr.next;
+            count++;
+        }
+        if (count == k) { // if k+1 node is found
+            curr = reverseKGroup(curr, k); // reverse list with k+1 node as head
+            // head - head-pointer to direct part,
+            // curr - head-pointer to reversed part;
+            while (count-- > 0) { // reverse current k-group:
+                ListNode tmp = head.next; // tmp - next head in direct part
+                head.next = curr; // preappending "direct" head to the reversed list
+                curr = head; // move head of reversed part to a new node
+                head = tmp; // move "direct" head to the next node in direct part
+            }
+            head = curr;
+        }
+        return head;
+    }
+}
